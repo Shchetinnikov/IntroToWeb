1974758533f91dd9681f215594ffd2e9d4b240f1
Shchetinnikov <kshchetinnikov@yandex.ru>
2022-08-11 03:10:36 +0300

    IntroToWeb

---
+++ backend/app/db/interaction/interaction.py
@@ -0,0 +1,81 @@
+# API сервер-БД
+
+from app.db.client.client import MySQLConnection
+from models.models import Base, User, MusicalComposition
+from exceptions import UserNotFoundException
+
+
+class DbInteraction:
+
+    def __init__(self, host, port, user, password, db_name, rebuild_db=False):
+        self.mysql_connection = MySQLConnection(
+            host = host,
+            user = user,
+            port = port,
+            password   = password,
+            db_name    = db_name,
+            rebuild_db = rebuild_db
+        )
+
+        self.engine = self.mysql_connection.connection.engine
+
+        if rebuild_db:
+            self.create_table_users()
+            self.create_table_musical_compositions()
+
+    def create_table_users(self):
+        if not self.engine.dialect.has_table(self.engine, 'user'):
+            Base.metadata.tables['users'].create(self.engine)
+        else:
+            self.mysql_connection.execute_query('DROP TABLE IF EXISTS users')
+            Base.metadata.tables['users'].create(self.engine)
+
+    def create_table_musical_compositions(self):
+        if not self.engine.dialect.has_table(self.engine, 'musical_compositions'):
+            Base.metadata.tables['musical_compositions'].create(self.engine)
+        else:
+            self.mysql_connection.execute_query('DROP TABLE IF EXISTS musical_compositions')
+            Base.metadata.tables['musical_compositions'].create(self.engine)
+
+    def add_user_info(self, username, email, password):
+        user = User(
+            username = username,
+            password = password,
+            email    = email
+        )
+        self.mysql_connection.session.add(user)
+        return self.get_user_info(username)
+
+    def get_user_info(self, username):
+        user = self.mysql_connection.session.query(User).filter_by(username=username).first()
+        if user:
+            self.mysql_connection.session.expire_all()
+            return {'username': user.username, 'email': user.email, 'password': user.password}
+        else:
+            raise UserNotFoundException('User is not found')
+
+    def edit_user_info(self, username, new_username=None, new_email=None, new_password=None):
+        user = self.mysql_connection.session.query(User).filter_by(username=username).first()
+        if user:
+            if new_username is not None:
+                user.username = new_username
+            if new_email is not None:
+                user.email = new_email
+            if new_password is not None:
+                user.password = new_password
+            return self.get_user_info(username if new_username is None else new_username)
+        else:
+            raise UserNotFoundException('User is not found')
+
+
+if __name__ == '__main__':
+    db = DbInteraction(
+        host = '127.0.0.1',
+        port = 3306,
+        user = 'root',
+        password   = 'pass',
+        db_name    = 'some_db',
+        rebuild_db = True
+    )
+
+    db.add_user_info('test', 'test', 'test')
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Web-programming | Practice
+Introduction to Web-programming | Practice
+
+## Frontend
+1. Course | Web programming for the school course "Computer Science" based on the book ["Computer Science. 11th grade. Advanced level: textbook at 2 h. H. 1" Polyakov K. Yu. / Eremin E. A.](https://kpolyakov.spb.ru/school/probook.htm)
+2. Store | Web programming for the school course "Computer Science" based on the book ["Computer Science. 11th grade. Advanced level: textbook at 2 h. H. 1" Polyakov K. Yu. / Eremin E. A.](https://kpolyakov.spb.ru/school/probook.htm)
+
+## Backend
+[Master class: "Backend in Python"](https://www.youtube.com/watch?v=LcZ9uJn8ffA&t=5s&ab_channel=%D0%98%D0%98%D0%9A%D0%A1%D0%9D%D0%98%D0%AF%D0%A3%D0%9C%D0%98%D0%A4%D0%98)
